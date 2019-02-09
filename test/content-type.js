bec62ad6f4663a71085569aba7e4e9ae18b59680
npm-simplified <simplified21@gmail.com>
2019-02-09 16:36:10 +0800

    Set content type queries

---
+++ test/content-type.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const assert = require('chai').assert,
+    _ = require('underscore');
+
+require('../lib/load');
+
+describe('Content type queries', () => {
+    it('Should create new content type table', async function() {
+        let done = await dbManager.createTable( 'content_type', {
+            name: {
+                type: 'str',
+                length: 60,
+                required: true,
+                index: true
+            },
+            slug: {
+                type: 'str',
+                length: 100,
+                required: true,
+                unique: true
+            },
+            type: {
+                type: 'enum',
+                enum: ['content', 'group'],
+                default: 'content'
+            },
+            status: {
+                type: 'enum',
+                enum: ['active', 'inactive', 'builtin'],
+                default: 'active'
+            },
+            hierarchical: {
+                type: 'bool'
+            },
+            archive: {
+                type: 'bool',
+            },
+            page: {
+                type: 'bool',
+            },
+            comments: {
+                type: 'bool'
+            },
+            rest: {
+                type: 'bool'
+            },
+            archiveTitle: {
+                type: 'str',
+                length: 160
+            },
+            archiveDescription: {
+                type: 'str',
+                length: 255
+            },
+            archiveSlug: {
+                type: 'str',
+                length: 60,
+                required: true
+            },
+            itemsPerPage: {
+                type: 'int',
+                length: 2,
+                default: 50
+            },
+            fields: {
+                type: 'object'
+            }
+        });
+
+        assert.isTrue(done);
+
+        return done;
+    });
+
+    it('Should drop content type table', async function() {
+        let done = await dbManager.dropTable('content_type');
+
+        assert.isTrue(done);
+
+        return done;
+    });
+});
